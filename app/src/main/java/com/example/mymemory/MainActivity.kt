c9fd319b7ada96f1d8e207ac60f1f6b204b95873
Fazeel Ahmed Khan <fazeelahmedkhan15@gmail.com>
2021-03-21 11:38:26 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mymemory/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mymemory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumMoves: TextView
+    private lateinit var tvNumPairs: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
